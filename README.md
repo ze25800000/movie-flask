6fa756633e2509fdcfb20916d561696c757e4e29
ze25800000 <ze258100000@sina.com>
2018-06-07 13:53:14 +0800

    3-7 管理员、登陆日志、操作日志数据模型设计

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 ![定义会员数据模型](https://github.com/ze25800000/movie-flask/blob/master/pic/%E5%AE%9A%E4%B9%89%E4%BC%9A%E5%91%98%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B.jpg?raw=true)
 ![定义会员登录日志数据模型](https://github.com/ze25800000/movie-flask/blob/master/pic/%E5%AE%9A%E4%B9%89%E4%BC%9A%E5%91%98%E7%99%BB%E5%BD%95%E6%97%A5%E5%BF%97%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B.jpg?raw=true)
 ![定义会员数据模型及关联](https://github.com/ze25800000/movie-flask/blob/master/pic/%E5%AE%9A%E4%B9%89%E4%BC%9A%E5%91%98%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B%E5%8F%8A%E5%85%B3%E8%81%94.jpg?raw=true)
-# 3-4 标签-电影-上映预告数据模型设计
 
+# 3-4 标签-电影-上映预告数据模型设计
 ![标签、电影、上映预告数据模型设计](https://github.com/ze25800000/movie-flask/blob/master/pic/%E6%A0%87%E7%AD%BE%E3%80%81%E7%94%B5%E5%BD%B1%E3%80%81%E4%B8%8A%E6%98%A0%E9%A2%84%E5%91%8A%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B%E8%AE%BE%E8%AE%A1.jpg?raw=true)
 ![定义电影数据模型](https://github.com/ze25800000/movie-flask/blob/master/pic/3-4-2%E5%AE%9A%E4%B9%89%E7%94%B5%E5%BD%B1%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B.jpg?raw=true)
 ![定义上映预告数据模型](https://github.com/ze25800000/movie-flask/blob/master/pic/3-4-3%E5%AE%9A%E4%B9%89%E4%B8%8A%E6%98%A0%E9%A2%84%E5%91%8A%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B.jpg?raw=true)
@@ -16,6 +16,52 @@
 ![3-5-1评论及收藏电影数据模型设计](https://github.com/ze25800000/movie-flask/blob/master/pic/3-5-1%E8%AF%84%E8%AE%BA%E5%8F%8A%E6%94%B6%E8%97%8F%E7%94%B5%E5%BD%B1%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B%E8%AE%BE%E8%AE%A1.jpg?raw=true)
 ![3-5-2定义收藏电影数据模型](https://github.com/ze25800000/movie-flask/blob/master/pic/3-5-2%E5%AE%9A%E4%B9%89%E6%94%B6%E8%97%8F%E7%94%B5%E5%BD%B1%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B.jpg?raw=true)
 
-# 3-6 评论及收藏电影数据模型设计
+# 3-6 权限及角色数据模型设计
 ![3-6-1权限及角色数据模型设计](https://github.com/ze25800000/movie-flask/blob/master/pic/3-6-1%E6%9D%83%E9%99%90%E5%8F%8A%E8%A7%92%E8%89%B2%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B%E8%AE%BE%E8%AE%A1.jpg?raw=true)
 ![3-6-2定义角色数据模型](https://github.com/ze25800000/movie-flask/blob/master/pic/3-6-2%E5%AE%9A%E4%B9%89%E8%A7%92%E8%89%B2%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B.jpg?raw=true)
+
+# 3-7 管理员、登录日志、操作日志数据模型设计
+![3-7-1管理员、登录日志、操作日志数据模型设计](https://github.com/ze25800000/movie-flask/blob/master/pic/3-7-1%E7%AE%A1%E7%90%86%E5%91%98%E3%80%81%E7%99%BB%E5%BD%95%E6%97%A5%E5%BF%97%E3%80%81%E6%93%8D%E4%BD%9C%E6%97%A5%E5%BF%97%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B%E8%AE%BE%E8%AE%A1.jpg?raw=true)
+![3-7-2定义管理员登录日志数据模型](https://github.com/ze25800000/movie-flask/blob/master/pic/3-7-2%E5%AE%9A%E4%B9%89%E7%AE%A1%E7%90%86%E5%91%98%E7%99%BB%E5%BD%95%E6%97%A5%E5%BF%97%E6%95%B0%E6%8D%AE%E6%A8%A1%E5%9E%8B.jpg?raw=true)
+![3-7-3定义操作日志](https://github.com/ze25800000/movie-flask/blob/master/pic/3-7-3%E5%AE%9A%E4%B9%89%E6%93%8D%E4%BD%9C%E6%97%A5%E5%BF%97.jpg?raw=true)
+
+### 执行models.py生成数据表
+1. 安装pymysql pip install pymysql
+2. import pymysql
+```
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Yangze@1234@127.0.0.1:3306/movie"
+```
+3.  创建表时，修改字符集
+```
+__table_args__ = {'mysql_collate': 'utf8_general_ci'}
+# 最下面加如下代码
+if __name__ == "__main__":
+     db.create_all()
+```
+4.  python models.py
+
+5. 插入role角色表字段
+```
+if __name__ == "__main__":
+    role = Role(
+        name="超级管理员",
+        auths=""
+    )
+    db.session.add(role)
+    db.session.commit()
+```
+
+6. 插入admin管理员字段
+```
+if __name__ == "__main__":
+    from werkzeug.security import generate_password_hash
+
+    admin = Admin(
+        name="admin",
+        pwd=generate_password_hash("yangze"),
+        is_super=0,
+        role_id=1
+    )
+    db.session.add(admin)
+    db.session.commit()
+```
--- app/models.py
+++ app/models.py
@@ -2,9 +2,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pymysql
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:Yangze@1234@127.0.0.1:3306/movie"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Yangze@1234@127.0.0.1:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 # 会员
 class User(db.Model):
     __tablename__ = "user"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     pwd = db.Column(db.String(100))
@@ -20,7 +22,7 @@ class User(db.Model):
     phone = db.Column(db.String(11), unique=True)
     info = db.Column(db.Text)
     face = db.Column(db.String(255), unique=True)
-    addtime = db.Column(db.String(100), index=True, default=datetime.utcnow)
+    addtime = db.Column(db.String(100), index=True, default=datetime.now)
     uuid = db.Column(db.String(255), unique=True)
     userlogs = db.relationship('Userlog', backref='user')  # 会员日志外键关联
     comments = db.relationship('Comment', backref='user')  # 评论外键关联
@@ -33,10 +35,11 @@ class User(db.Model):
 # 会员登录日志
 class Userlog(db.Model):
     __tablename__ = "userlog"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     ip = db.Column(db.String(100))
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return '<Userlog %r>' % self.id
@@ -45,9 +48,10 @@ class Userlog(db.Model):
 # 标签模型
 class Tag(db.Model):
     __tablename__ = "tag"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
     movies = db.relationship("Movie", backref='tag')  # 电影外键关联
 
     def __repr__(self):
@@ -57,6 +61,7 @@ class Tag(db.Model):
 # 电影
 class Movie(db.Model):
     __tablename__ = "movie"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), unique=True)
     url = db.Column(db.String(255), unique=True)
@@ -69,7 +74,7 @@ class Movie(db.Model):
     area = db.Column(db.String(255))
     release_time = db.Column(db.Date)
     length = db.Column(db.String(100))
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
     comments = db.relationship("Comment", backref='movie')  # 评论外键关联
     moviecols = db.relationship('Moviecol', backref='movie')  # 收藏外键关联
 
@@ -80,10 +85,11 @@ class Movie(db.Model):
 # 上映预告
 class Preview(db.Model):
     __tablename__ = "preview"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), unique=True)
     logo = db.Column(db.String(255), unique=True)
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return "<Preview %r>" % self.title
@@ -92,11 +98,12 @@ class Preview(db.Model):
 # 评论
 class Comment(db.Model):
     __tablename__ = "comment"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return "<Comment %r>" % self.id
@@ -105,10 +112,11 @@ class Comment(db.Model):
 # 电影收藏
 class Moviecol(db.Model):
     __tablename__ = "moviecol"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return "<Moviecol %r>" % self.id
@@ -117,10 +125,11 @@ class Moviecol(db.Model):
 # 权限
 class Auth(db.Model):
     __tablename__ = "auth"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     url = db.Column(db.String(255), unique=True)
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return "<Auth %r>" % self.name
@@ -129,10 +138,11 @@ class Auth(db.Model):
 # 角色
 class Role(db.Model):
     __tablename__ = "role"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     auths = db.Column(db.String(100))
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return "<Role %r>" % self.name
@@ -141,12 +151,13 @@ class Role(db.Model):
 # 管理员
 class Admin(db.Model):
     __tablename__ = "admin"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     pwd = db.Column(db.String(100))
     is_super = db.Column(db.SmallInteger)
     role_id = db.Column(db.Integer, db.ForeignKey('role.id'))  # 所属角色
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
     adminlogs = db.relationship("Adminlog", backref='admin')  # 管理员日志外键关联
     oplogs = db.relationship("Oplog", backref='admin')  # 管理员操作日志外键关联
 
@@ -157,10 +168,11 @@ class Admin(db.Model):
 # 管理员登录日志
 class Adminlog(db.Model):
     __tablename__ = "adminlog"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
     ip = db.Column(db.String(100))
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return "<Adminlog %r>" % self.id
@@ -169,15 +181,25 @@ class Adminlog(db.Model):
 # 操作日志
 class Oplog(db.Model):
     __tablename__ = "oplog"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
     id = db.Column(db.Integer, primary_key=True)
     admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
     ip = db.Column(db.String(100))
     reason = db.Column(db.String(600))
-    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.now)
 
     def __repr__(self):
         return "<Adminlog %r>" % self.id
 
 
-if __name__ == "__main":
-    db.create_all()
+if __name__ == "__main__":
+    from werkzeug.security import generate_password_hash
+
+    admin = Admin(
+        name="admin1",
+        pwd=generate_password_hash("yangze2"),
+        is_super=0,
+        role_id=1
+    )
+    db.session.add(admin)
+    db.session.commit()
